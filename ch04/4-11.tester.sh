a4272bc68370c30d517a8553b8722b7b3acaca3b
KeisLuv5991 <chmhj9997@gmail.com>
2022-01-29 23:05:49 +0900

    update tester to recieve start dir, iter-num

--- ch04/4-11.tester.sh
+++ ch04/4-11.tester.sh
@@ -1,15 +1,24 @@
-tot=0
-execute=$1
-n=$2
+start_directory=$1
+iteration_num=$2
 
-for((i=0;i<$n;i++))
-do
-	echo -ne "\033[1K\r"
-	echo -n "$i / $n"
-	t=$(/usr/bin/time --format="%e" sudo $execute / 2>&1 > /dev/null)
-	tot=$(echo "$tot + $t" | bc -l)
-done
+function exec_and_time(){
+	tot=0
+	for((i=0;i<$iteration_num;i++))
+	do
+		echo -ne "\033[1K\r"
+		echo -n "$i / $iteration_num"
+		t=$(/usr/bin/time --format="%e" $1 $start_directory 2>&1 > /dev/null)
+		tot=$(echo "$tot + $t" | bc -l)
+	done
 
-echo -e "\033[2K\r"
-echo "$tot / $n" | bc -l 
+	echo -e "\033[2K\r"
+	echo "$tot / $iteration_num" | bc -l 
+}
+chdir_ver="./4-11"
+origin_ver="./4-11.origin"
+
+exec_and_time $chdir_ver
+exec_and_time $origin_ver
+
+
 
--- ch04/test.sh
+++
@@ -1,3 +0,0 @@
-. 4-11.tester.sh ./4-11 100 
-. 4-11.tester.sh ./4-11.origin 100
-
